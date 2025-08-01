e7a849912da428eccb813717c61710e3c44bd8fb
Victor Capillo <82743101+vcapillo@users.noreply.github.com>
2025-08-01 07:05:28 +0000

    Agregado filtro datetimeformat para fixtures

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from sofascore_api import get_team_fixtures, get_standings
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -16,6 +17,16 @@ def fixtures():
 def standings():
     data = get_standings()
     return render_template("standings.html", standings=data, active_page="standings")
+
+@app.template_filter("datetimeformat")
+def datetimeformat(value, format="%d-%m-%Y %H:%M"):
+    """
+    Convierte timestamp en formato legible
+    Ejemplo: 1693449600 -> '31-08-2025 15:00'
+    """
+    if not value:
+        return ""
+    return datetime.fromtimestamp(int(value)).strftime(format)
 
 if __name__ == "__main__":
     app.run(debug=True)
